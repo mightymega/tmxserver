f93240ef7b3833eecfa3969295f61d215280e206
mighty mega <123591644+mightymega@users.noreply.github.com>
2025-01-15 09:10:53 +0300

    Create index.php

---
+++ templates/index.php
@@ -0,0 +1,16 @@
+<?php
+include '../config/db_config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="../static/css/style.css">
+</head>
+<body>
+    <h1>Welcome to My Site</h1>
+    <a href="login.php">Login</a> | <a href="signup.php">Signup</a>
+</body>
+</html>
